c2b7a002ae1b96a8670eed7107473185da641dd0
WillTKaufhold <wtk23@cam.ac.uk>
2019-12-31 12:14:00 +0000

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+# /home/wtk23/anaconda3/bin/python
+import mrdna
+import pandas as pd
+
+
+
+
+#first we set up the different segments
+
+#D1 segment
+mrdna.DoubleStrandedSegment(name=,start_position=,end_position=,num_bp=)
+
+#D5 segment
